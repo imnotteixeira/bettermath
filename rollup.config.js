068e3a99da43009c209f346924026faddae2dc11
Angelo Teixeira <imnotteixeira@gmail.com>
2023-11-12 01:14:38 +0100

    feat(bettermath): Add rollup bundling (ES Modules) + bump to TS@5

---
+++ rollup.config.js
@@ -0,0 +1,15 @@
+import typescript from '@rollup/plugin-typescript';
+import terser from '@rollup/plugin-terser';
+
+export default {
+	input: 'src/index.ts',
+	output: {
+		dir: 'dist',
+		format: 'es',
+        plugins: [terser()]
+	},
+    plugins: [typescript()],
+    treeshake: {
+        moduleSideEffects: false
+    }
+};
