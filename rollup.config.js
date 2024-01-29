f48bdc5dfda992bbfc7fbf1dbadb24dc91e3f8ef
Angelo Teixeira <imnotteixeira@gmail.com>
2024-01-29 23:51:38 +0100

    build(bettermath): Stop minifying code and tree-shaking, leaving that duty to dependents

--- rollup.config.js
+++ rollup.config.js
@@ -1,15 +1,12 @@
 import typescript from '@rollup/plugin-typescript';
-import terser from '@rollup/plugin-terser';
 
 export default {
 	input: 'src/index.ts',
 	output: {
 		dir: 'dist',
 		format: 'es',
-        plugins: [terser()]
 	},
-    plugins: [typescript()],
-    treeshake: {
-        moduleSideEffects: false
-    }
+    plugins: [typescript({
+        noEmitOnError: true
+    })],
 };
